ca8614dd03f9378e7c6c1ca8c3cece077486ce27
Gabriele Tozzi <tozzigabriele7@gmail.com>
2021-02-09 18:33:17 +0100

    Aggiunto Database

---
+++ file/Database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS EsercizioTPSIT;
+USE EsercizioTPSIT;
+CREATE TABLE IF NOT EXISTS `eserciziotpsit`.`studenti` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cognome` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+  INSERT INTO Studenti (id, nome, cognome)
+  VALUES (1,"Andrea", "Buono"),
+  (2,"Stefano", "Caiazzo"),
+  (3,"Lorenzo", "Cibecchini"),
+  (4,"Dario", "Colzi"),
+  (5,"Gabriele", "Tozzi");
